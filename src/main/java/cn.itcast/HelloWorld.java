6a6dae1acc3a71132f1ceff4a243e498e71f7b19
tiancailyb <2224615397@qq.com>
2020-02-20 11:06:31 +0800

    start

---
+++ src/main/java/cn.itcast/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * @author lyb
+ * @date 2020/2/20
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    /**
+     * 计算两个数的和
+     * @param a
+     * @param b
+     */
+    public void sum(int a, int b) {
+        System.out.println(a + b);
+    }
+}
---
+++ src/main/java/cn/core/three/Test.java
@@ -0,0 +1,8 @@
+package cn.core.three;
+
+/**
+ * @author lyb
+ * @date 2020/2/20
+ */
+public class Test {
+}
