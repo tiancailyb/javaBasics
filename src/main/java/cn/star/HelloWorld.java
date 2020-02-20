d0fd6648670bb051263e4435987a6d24525572df
tiancailyb <2224615397@qq.com>
2020-02-20 10:47:56 +0800

    第二次提交

--- src/main/java/cn/star/HelloWorld.java
+++ src/main/java/cn/star/HelloWorld.java
@@ -7,5 +7,6 @@ package cn.star;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world");
+        System.out.println("hahaha");
     }
 }
