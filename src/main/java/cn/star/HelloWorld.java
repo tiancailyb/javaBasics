4be470efafa311c9c5722a19f0172c9716573462
tiancailyb <2224615397@qq.com>
2020-02-20 10:15:03 +0800

    第一次提交

---
+++ src/main/java/cn/star/HelloWorld.java
@@ -0,0 +1,11 @@
+package cn.star;
+
+/**
+ * @author lyb
+ * @date 2020/2/20
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
